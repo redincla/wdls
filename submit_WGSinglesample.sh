edab6d1b541596919c125544615719e8952006fd
Redin Claire <claire.redin@chuv.ch>
2020-10-09 16:17:26 +0200

    update single sample input file

--- submit_WGSinglesample.sh
+++ submit_WGSinglesample.sh
@@ -9,10 +9,7 @@
 
 module add Development/java/1.8.0_232
 module add R/3.6.1
-module add UHTS/Analysis/samtools/1.10
-module add UHTS/Analysis/GenomeAnalysisTK/4.1.3.0
-module add UHTS/Analysis/picard-tools/2.21.8
 
 echo "hello from `hostname` at `date`"
 
-java -Dconfig.file=/home/credin/.cromwell.conf -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/WholeGenomeGermlineSingleSample.wdl -i /home/credin/scratch/WGS/wdls/WholeGenomeGermlineSingleSample-E1.json
+java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/WholeGenomeGermlineSingleSample.wdl -i /home/credin/scratch/WGS/wdls/WholeGenomeGermlineSingleSample.json
