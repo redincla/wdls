99ec53410d25602eb5acb7eb6c5ba48c619c2dc5
Redin Claire <claire.redin@chuv.ch>
2021-02-22 15:45:59 +0100

    add prefix option

--- imports/launch-peddy.sh
+++ imports/launch-peddy.sh
@@ -35,4 +35,4 @@ cd ${OUTDIR}
 sbatch -J Peddy -D ${OUTDIR} --time 60 -p normal \
   		-n 1 \
   		--mem-per-cpu=2G \
- 	    --wrap "peddy --plot -p 3 --sites hg38 ${vcf_file} ${ped_file}"
+ 	    --wrap "peddy --plot -p 3 --sites hg38 --prefix TACG.CoPrAc ${vcf_file} ${ped_file}"
