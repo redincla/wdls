43bf633ea02be073542e2d229143d40451cb59c5
Redin Claire <claire.redin@chuv.ch>
2021-05-26 15:11:46 +0200

    Fix input parameters

--- imports/workflows/launch-AnnotSV.sh
+++ imports/workflows/launch-AnnotSV.sh
@@ -9,10 +9,12 @@ export ANNOTSV=/home/credin/refs/tools/AnnotSV  #add ANNOTSV to path
 module add UHTS/Analysis/samtools/1.10  #load bcftools
 module load UHTS/Analysis/BEDTools/2.29.2 #load bedtools
 
-$ANNOTSV/bin/AnnotSV -SvinputFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/TACG_COPRAC_1120.delly.vcf.gz \
+$ANNOTSV/bin/AnnotSV -SvinputFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/TACG_COPRAC_HEV_VZV_0421.delly.vcf.gz \
     -genomeBuild GRCh38 \
+    -minTotalNumber 100 \
+    -outputFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/TACG_COPRAC_HEV_0421.delly.annotSV_full+PASS.tsv \
     -candidateGenesFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/CAR_IFN.list \
-    -candidateGenesFiltering yes \
-    -snvIndelFiles /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/JointCalling/TACG_CoPRACrefined_priors-filtered.vcf.gz \
+    -candidateGenesFiltering no \
+    -snvIndelFiles /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/JointCalling/TACG_CoPRAC_HEVrefined_priors-filtered.vcf.gz \
     -snvIndelPASS 1 \
-    -outputFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/TACG_COPRAC_1120.delly.annotSV_CAR+IFN_SNVcheck.tsv
+#     -rankFiltering "3,4,5" \
