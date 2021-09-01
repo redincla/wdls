dde6128f1d10c909dd69bfff895563ceaece33e8
Redin Claire <claire.redin@chuv.ch>
2021-09-01 17:00:03 +0200

    fix json files

--- imports/workflows/runDelly-sample.sh
+++ imports/workflows/runDelly-sample.sh
@@ -7,4 +7,4 @@
 
 module add Development/java/1.8.0_232
 
-java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runDelly-sample.wdl -i /home/credin/scratch/WGS/wdls/imports/config-files/runDelly.inputs.json -o /home/credin/scratch/WGS/wdls/imports/config-files/runDelly.options.json
+java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runDelly-sample.wdl -i /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/runDelly.inputs.json -o /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/runDelly.options.json
