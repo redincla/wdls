aaab5cbdc381be2508086e0fb52ddc36073274b6
Redin Claire <claire.redin@chuv.ch>
2021-09-01 16:38:56 +0200

    concatenate fq and encrypt for ftp upload

--- imports/launch-encrypt.sh
+++ imports/launch-encrypt.sh
@@ -11,10 +11,10 @@ echo "#!/bin/bash
 #SBATCH --mem-per-cpu=2G
 #SBATCH --output=/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/${sample_ID}.encrypt.slurm.%N.%j.log
 
-cat /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/${sample_ID}/Raw/*R1_001.fastq.gz > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/${sample_ID}_R1_001.fq.gz
-cat /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/${sample_ID}/Raw/*R2_001.fastq.gz > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/${sample_ID}_R2_001.fq.gz
-openssl aes-256-cbc -e -in /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/${sample_ID}_R1_001.fq.gz -out /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/crypted/${sample_ID}_R1_001_crypted.fq.gz -pass file:/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/keyfile_batch2
-openssl aes-256-cbc -e -in /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/${sample_ID}_R2_001.fq.gz -out /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/crypted/${sample_ID}_R2_001_crypted.fq.gz -pass file:/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/Batch3/keyfile_batch2" > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/${sample_ID}.encrypt.script-submit
+cat ${sample_ID}*R1_001.fastq.gz > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/${sample_ID}_R1_001.fq.gz
+cat ${sample_ID}*R2_001.fastq.gz > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/${sample_ID}_R2_001.fq.gz
+openssl aes-256-cbc -e -in /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/${sample_ID}_R1_001.fq.gz -out /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/crypted/${sample_ID}_R1_001_crypted.fq.gz -pass file:/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/keyfile_batch3_crypted
+openssl aes-256-cbc -e -in /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/${sample_ID}_R2_001.fq.gz -out /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/crypted/${sample_ID}_R2_001_crypted.fq.gz -pass file:/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/RFU_FQs/keyfile_batch3_crypted" > /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/${sample_ID}.encrypt.script-submit
 
 sbatch /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/${sample_ID}.encrypt.script-submit
 
