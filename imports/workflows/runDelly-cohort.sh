575f01aae004cce2c0ebd29ab35bed8ff0167da4
Redin Claire <claire.redin@chuv.ch>
2021-09-01 16:55:47 +0200

    update json files

--- imports/workflows/runDelly-cohort.sh
+++ imports/workflows/runDelly-cohort.sh
@@ -6,9 +6,6 @@
 #SBATCH --output=/home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/logs/slurm.Delly-FG.%N.%j.log
 
 module add Development/java/1.8.0_232
-module add R/3.6.1
-module add UHTS/Analysis/samtools/1.10
-module add UHTS/Analysis/GenomeAnalysisTK/4.1.3.0
-module add UHTS/Analysis/picard-tools/2.21.8
 
-java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runDelly-cohort.wdl -i /home/credin/scratch/WGS/wdls/imports/config-files/runDelly.inputs.json -o /home/credin/scratch/WGS/wdls/imports/config-files/runDelly.options.json
+
+java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runDelly-cohort.wdl -i /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/runDelly.inputs.json -o /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/runDelly.options.json
