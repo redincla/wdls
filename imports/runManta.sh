721e6e2eb2f7f076aa952377915dfa205fb21180
Redin Claire <claire.redin@chuv.ch>
2021-09-01 16:41:12 +0200

    update json files

--- imports/runManta.sh
+++ imports/runManta.sh
@@ -7,4 +7,4 @@
 
 module add Development/java/1.8.0_232
 
-java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runManta.wdl -i /home/credin/scratch/WGS/wdls/imports/config-files/Manta.inputs.json -o /home/credin/scratch/WGS/wdls/imports/config-files/Manta.options.json
+java -Dconfig.file=/home/credin/.cromwell.conf_new -jar /software/Utility/cromwell/47/bin/cromwell-47.jar  run /home/credin/scratch/WGS/wdls/imports/workflows/runManta.wdl -i /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/Manta.inputs.json -o /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/json/Manta.options.json
