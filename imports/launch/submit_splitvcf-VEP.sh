8609e32cca037a9b75fc9a8cbc3621c4dcc3e4c7
Redin Claire <claire.redin@chuv.ch>
2022-02-15 10:40:07 +0100

    Add VEP to nomenclature

--- imports/launch/submit_splitvcf-VEP.sh
+++ imports/launch/submit_splitvcf-VEP.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH --job-name=SplitVCF
+#SBATCH --job-name=SplitVCF-VEP
 #SBATCH -t 4-00:00
 #SBATCH --mem-per-cpu=10G
-#SBATCH -o /users/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/logs/splitVCF.slurm.%N.%j.log
+#SBATCH -o /users/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/logs/splitVCF-VEP.slurm.%N.%j.log
 
 # checks for appropriate input parameters
 if [ $# -eq 2 ]; then
- input_file=$1 #full path to json input file for FilterVCF.wdl workflow
- options_file=$2 #full path to json options file for FilterVCF.wdl workflow
+ input_file=$1 #full path to json input file for FilterVCF-VEP.wdl workflow
+ options_file=$2 #full path to json options file for FilterVCF-VEP.wdl workflow
 
 else
- echo -e "\n\nSubmit split and filter VCF workflow (FilterVCF.wdl) \n\nAuthor: Claire Redin (claire.redin@chuv.ch)\n\n"
+ echo -e "\n\nSubmit split and filter VCF workflow (FilterVCF-VEP.wdl) \n\nAuthor: Claire Redin (claire.redin@chuv.ch)\n\n"
  echo "Usage:"
- echo "sbatch submit_split.vcf.sh [input_file] [option_file] "
+ echo "sbatch submit_splitvcf-VEP.vcf.sh [input_file] [option_file] "
  echo "input_file: full path to input json file"
  echo "option_file: full path to options json file"
  exit 1
