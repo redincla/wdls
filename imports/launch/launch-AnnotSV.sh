37639b245bdffaf21ffe677bb2f5dac359a33c1f
Redin Claire <claire.redin@chuv.ch>
2022-05-02 10:51:40 +0200

    update parameters

--- imports/launch/launch-AnnotSV.sh
+++ imports/launch/launch-AnnotSV.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1
 #SBATCH --job-name=annotSV
 #SBATCH -t 2-00:00
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=15G
 #SBATCH --output=AnnotSV.slurm.%N.%j.log
     
 export ANNOTSV=/home/credin/refs/tools/AnnotSV  #add ANNOTSV to path
@@ -10,14 +10,15 @@ module add UHTS/Analysis/samtools/1.10  #load bcftools
 module load UHTS/Analysis/BEDTools/2.29.2 #load bedtools
 
 input_vcf=$1
+output_file=$2
 
 $ANNOTSV/bin/AnnotSV -SvinputFile ${input_vcf} \
     -genomeBuild GRCh38 \
     -minTotalNumber 100 \
-    -outputFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/211202_AnnotSV/249WGS_1221.delly.filtered.annotSV_3-4-5.tsv \
+    -outputFile ${output_file} \
     -candidateGenesFile /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/Delly/Full_Genome/CAR_IFN.list \
     -candidateGenesFiltering no \
-    -snvIndelFiles /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/JointCalling/211105_256WGSrefined_priors-filtered.vcf.gz \
+    -snvIndelFiles /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/JointCalling/220204_278WGSrefined_priors-filtered.vcf.gz \
     -snvIndelPASS 1 \
     -rankFiltering "3,4,5"
     #    -candidateSnvIndelFiles /home/credin/scratch/WGS/data_and_refs/data/Raw_FQ/TCAG/0920/JointCalling/210617_TACG_CoPRAC_HEV_VZVrefined_priors-filtered.vcf.gz \
